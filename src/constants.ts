e774f02686f6d5eea604d935d8a452bfb1b492ef
David Fross <davidmfross@gmail.com>
2024-09-03 10:19:27 -0500

    fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "DavidFross.com";
-export const SITE_DESCRIPTION = "Personal website of David Fross";
+export const SITE_TITLE = 'DavidFross.com'
+export const SITE_DESCRIPTION = 'Personal website of David Fross.'

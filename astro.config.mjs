64324add1eb4b9af5a0c027eff0f6d3759daef09
David Fross <davidmfross@gmail.com>
2024-09-13 12:15:17 -0500

    Added sitemap and robot

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-
 import icon from 'astro-icon'
-
-import partytown from '@astrojs/partytown';
-
+import partytown from '@astrojs/partytown'
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon(), partytown()]
+	site: 'https://www.davidfross.com/',
+	integrations: [tailwind(), icon(), partytown(), sitemap()],
 })

4248191c6ffd38fd18f491518bc05402c63a9e55
David Fross <davidmfross@gmail.com>
2024-09-13 14:56:19 -0500

    tweaks

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'David Fross'
 export const SITE_DESCRIPTION =
-	'David Fross. Maker of digital products and websites that are accessible, a joy to use, and perform exceedingly well.'
+	'David Fross. Designer and developer of digital products and websites that are accessible, a joy to use, and perform exceedingly well.'

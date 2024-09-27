dc33ad61db5737e13d2b8c04a9c4986e3cb4aebe
David Fross <davidmfross@gmail.com>
2024-09-27 09:50:44 -0500

    performance tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import partytown from '@astrojs/partytown'
+import compressor from 'astro-compressor'
+import playformCompress from '@playform/compress'
+import playformInline from '@playform/inline'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.davidfross.com/',
-	integrations: [tailwind(), icon(), partytown(), sitemap()],
+	integrations: [tailwind(), icon(), partytown(), sitemap(), playformCompress(), playformInline(), compressor()],
 })

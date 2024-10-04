3cc95f60a34aa12f25e896d279bcc4912dc1a011
David Fross <davidmfross@gmail.com>
2024-10-04 09:09:00 -0500

    package updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import partytown from '@astrojs/partytown'
-import compressor from 'astro-compressor'
 import playformCompress from '@playform/compress'
 import playformInline from '@playform/inline'
-// https://astro.build/config
+import compressor from 'astro-compressor'
+
 export default defineConfig({
 	site: 'https://www.davidfross.com/',
 	integrations: [tailwind(), icon(), partytown(), sitemap(), playformCompress(), playformInline(), compressor()],

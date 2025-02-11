dda5f3bdf70d052e0836f9015cc6bb0e9145a412
David Fross <davidmfross@gmail.com>
2025-02-11 15:09:12 -0600

    upgrade to tailwind 4, package updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import partytown from '@astrojs/partytown'
 import playformCompress from '@playform/compress'
 import playformInline from '@playform/inline'
 import compressor from 'astro-compressor'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-	site: 'https://www.davidfross.com/',
-	prefetch: {
-		prefetchAll: true,
+  site: 'https://www.davidfross.com/',
+
+  prefetch: {
+      prefetchAll: true,
 	},
-	integrations: [tailwind(), icon(), partytown(), sitemap(), playformInline(), playformCompress(), compressor()],
+
+  integrations: [icon(), partytown(), sitemap(), playformInline(), playformCompress(), compressor()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })

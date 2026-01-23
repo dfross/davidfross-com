fefd6e511cf1e3b05a3e7d528f2c7183ccd6d001
David Fross <davidmfross@gmail.com>
2026-01-23 15:23:25 -0600

    feat: add manifest.json, update layouts and styles

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,11 @@
 export const SITE_TITLE = 'David Fross'
 export const SITE_DESCRIPTION =
 	'David Fross. Designer and developer of digital products and websites that are accessible, a joy to use, and perform exceedingly well.'
+
+export const AUTHOR_NAME = 'David Fross'
+export const AUTHOR_JOB_TITLE = 'Designer & Developer'
+
+export const SOCIAL_LINKS = {
+	linkedin: 'https://www.linkedin.com/in/davidfross/',
+	email: 'davidmfross@gmail.com',
+} as const
